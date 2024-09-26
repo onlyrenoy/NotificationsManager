ab8c2060221770a42deb6db6a8c7c722d584af4f
Renoy <chowdhury-ext@westpole.it>
2024-09-26 16:09:28 +0200

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Renoy Chowdhury on 26/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
